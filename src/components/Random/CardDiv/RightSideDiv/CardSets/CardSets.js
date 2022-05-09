7599d8eab246efa271eeb7ba8497616c7fe7c280
Brandon <37314885+Sycendin@users.noreply.github.com>
2022-05-09 01:51:40 -0400

    style: update responsiveness of style for button

--- src/components/Random/CardDiv/RightSideDiv/CardSets/CardSets.js
+++ src/components/Random/CardDiv/RightSideDiv/CardSets/CardSets.js
@@ -2,15 +2,17 @@ import React, { Fragment } from "react";
 import "./CardSets.css";
 import CardSetsData from "./CardSetsData/CardSetsData";
 const CardSets = ({ cardData }) => {
-  return (
-    <Fragment>
-      <p className="sets-title">Sets:</p>
-      <div className="sets-div">
-        {cardData.card_sets.map((set, i) => {
-          return <CardSetsData key={i} set={set} />;
-        })}
-      </div>
-    </Fragment>
-  );
+  if (cardData.sets) {
+    return (
+      <Fragment>
+        <p className="sets-title">Sets:</p>
+        <div className="sets-div">
+          {cardData.card_sets.map((set, i) => {
+            return <CardSetsData key={i} set={set} />;
+          })}
+        </div>
+      </Fragment>
+    );
+  }
 };
 export default CardSets;

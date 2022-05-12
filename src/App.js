4d728df92d3ff7670f31cc693a53d3724aa14387
Brandon <37314885+Sycendin@users.noreply.github.com>
2022-05-12 00:50:38 -0400

    update: add intial files for game page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import NotFound from "./components/NotFound/NotFound";
 import Home from "./components/Home/Home";
 import Random from "./components/Random/Random";
 import Search from "./components/Search/Search";
+import Game from "./components/Game/Game";
 function App() {
   return (
     <div className="App">
@@ -13,6 +14,7 @@ function App() {
           <Route path="/game/" element={<Home />} />
           <Route path="/game/random/" element={<Random />} />
           <Route path="/game/search/" element={<Search />} />
+          <Route path="/game/game/" element={<Game />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/components/Game/Game.js
@@ -0,0 +1,7 @@
+import React, { Fragment } from "react";
+import "./Game.css";
+const Game = () => {
+  return <Fragment></Fragment>;
+};
+
+export default Game;

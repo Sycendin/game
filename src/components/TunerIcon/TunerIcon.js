ec77e2ad7c53d7aac0a5efa8ad18f63a016b7a60
Brandon <37314885+Sycendin@users.noreply.github.com>
2022-05-16 02:54:39 -0400

    feat: tuner icon and monster type now displays Tuner icon displays for tuner monsters, the type of the monster displays for that monster and fix bug with token outline

--- src/components/TunerIcon/TunerIcon.js
+++ src/components/TunerIcon/TunerIcon.js
@@ -1,50 +1,20 @@
 import React, { Fragment } from "react";
 import "./TunerIcon.css";
-const TunerIcon = ({ attribute, display }) => {
-  let icon = "";
+const TunerIcon = ({ display }) => {
+  let icon =
+    "https://cdn.discordapp.com/attachments/958975018816131132/975650301992390696/tuner.webp";
   let cName = "";
   if (display === "search") {
     cName = "search-tuner-icon";
   } else {
     cName = "tuner-icon";
   }
-  switch (attribute) {
-    case "DARK":
-      icon =
-        "https://cdn.discordapp.com/attachments/958975018816131132/970903429876699176/dark.webp";
-      break;
-    case "LIGHT":
-      icon =
-        "https://cdn.discordapp.com/attachments/958975018816131132/970903430098976798/light.webp";
-      break;
-    case "FIRE":
-      icon =
-        "https://cdn.discordapp.com/attachments/958975018816131132/970903429054599188/fire.webp";
-      break;
-    case "WATER":
-      icon =
-        "https://cdn.discordapp.com/attachments/958975018816131132/970903430677807124/water.webp";
-      break;
-    case "WIND":
-      icon =
-        "https://cdn.discordapp.com/attachments/958975018816131132/970903429297881088/wind.webp";
-      break;
-    case "EARTH":
-      icon =
-        "https://cdn.discordapp.com/attachments/958975018816131132/970903429541163009/earth.webp";
-      break;
-    case "DIVINE":
-      icon =
-        "https://cdn.discordapp.com/attachments/958975018816131132/970903428836491334/divine.webp";
-      break;
-    default:
-      break;
-  }
+
   return (
     <Fragment>
       <img
         className={cName}
-        alt="attribute"
+        alt="tuner-icon"
         height={64}
         width={64}
         src={icon}

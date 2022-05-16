a7ea61477726f4b9db9e8d6de481f8cafb9782f4
Brandon <37314885+Sycendin@users.noreply.github.com>
2022-05-16 03:24:47 -0400

    feat: add archetype button Archetype button searches by archetype entered

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,13 +1,17 @@
 import React, { Fragment, useState } from "react";
 import "./Search.css";
-
 import Searchbox from "./Searchbox.js/Searchbox";
 import SearchImage from "./SearchImage/SearchImage";
 
 const Search = () => {
   const [options, setOptions] = useState([]);
   const [cardImage, setCardImage] = useState("");
+  const [archetypeSearch, setArchetypeSearch] = useState(false);
+  const [searchButtonClass, setSearchButtonClass] =
+    useState("archetype-button");
 
+  let searchUrl = "";
+  // Debounce function that limits the amount of times a function will be able to run
   function debounce(func, wait, immediate) {
     var timeout;
     return function () {
@@ -25,17 +29,28 @@ const Search = () => {
   }
 
   function onInputChange(event) {
+    // Clear card image on input change of searchbar
     setCardImage("");
+    //  Show no searchlist on empty input
     if (event.target.value === "") {
       setOptions([]);
     }
     if (event.target.value !== "") {
-      fetch(
-        `https://db.ygoprodeck.com/api/v7/cardinfo.php?fname=${event.target.value.replace(
+      // Change URL that will be sent to API depending on whether the archetype button is pressed
+      if (archetypeSearch === true) {
+        searchUrl = `https://db.ygoprodeck.com/api/v7/cardinfo.php?archetype=${event.target.value.replace(
           " ",
           "%20"
-        )}`
-      )
+        )}`;
+      } else {
+        searchUrl = `https://db.ygoprodeck.com/api/v7/cardinfo.php?fname=${event.target.value.replace(
+          " ",
+          "%20"
+        )}`;
+      }
+      // Call API and set information from it, if no results then set that to be displayed instead
+
+      fetch(searchUrl)
         .then((res) => res.json())
         .then((result) => {
           setOptions(result.data.slice(0, 10));
@@ -45,6 +60,19 @@ const Search = () => {
         });
     }
   }
+
+  // If search by archetype button is pressed then change class of it's button to show it is pressed/unpressed
+  // Then set the state for it appropiately
+  const changeArchetypeSearch = () => {
+    if (archetypeSearch === false) {
+      setSearchButtonClass("archetype-button-pressed");
+      setArchetypeSearch(true);
+    } else {
+      setSearchButtonClass("archetype-button");
+      setArchetypeSearch(false);
+    }
+  };
+  // Set store card image to state and clear searchlist
   const newImage = (image) => {
     setCardImage(image);
     setOptions([]);
@@ -52,9 +80,15 @@ const Search = () => {
   return (
     <Fragment>
       <h1>Search a card</h1>
-
+      <button
+        className={searchButtonClass}
+        onClick={() => changeArchetypeSearch()}
+      >
+        Search by Archetype
+      </button>
       <Searchbox
         data={options}
+        // Call debounce function with inputchange so that it doesn't run more than 4 times a second
         onInputChange={debounce(onInputChange, 250)}
         newImage={newImage}
       />

60c38452935b39f60fb718654bd4d6b7dcf91b9e
Brandon <37314885+Sycendin@users.noreply.github.com>
2022-05-20 00:42:20 -0400

    feat: game lets you search for fusion monsters Game uses searchbox component to search only fusion monsters  for now, one fusion monster is randomly selected from api call of all fusion monsters

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,7 +1,123 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect, useState } from "react";
+import Loading from "../Random/Loading/Loading";
+import Searchbox from "../Search/Searchbox.js/Searchbox";
 import "./Game.css";
 const Game = () => {
-  return <Fragment></Fragment>;
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [cards, setCards] = useState([]);
+  const [options, setOptions] = useState([]);
+  const [cardImage, setCardImage] = useState("");
+  const [archetypeSearch, setArchetypeSearch] = useState(false);
+  const [searchButtonClass, setSearchButtonClass] =
+    useState("archetype-button");
+
+  let searchUrl = "";
+  let cardSelect = 0;
+
+  function debounce(func, wait, immediate) {
+    var timeout;
+    return function () {
+      var context = this,
+        args = arguments;
+      var later = function () {
+        timeout = null;
+        if (!immediate) func.apply(context, args);
+      };
+      var callNow = immediate && !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+      if (callNow) func.apply(context, args);
+    };
+  }
+  function onInputChange(event) {
+    // Clear card image on input change of searchbar
+    setCardImage("");
+    //  Show no searchlist on empty input
+    if (event.target.value === "") {
+      setOptions([]);
+    }
+    if (event.target.value !== "") {
+      // Change URL that will be sent to API depending on whether the archetype button is pressed
+      if (archetypeSearch === true) {
+        searchUrl = `https://db.ygoprodeck.com/api/v7/cardinfo.php?archetype=${event.target.value.replace(
+          " ",
+          "%20"
+        )}`;
+      } else {
+        searchUrl = `https://db.ygoprodeck.com/api/v7/cardinfo.php?format=tcg&type=fusion%20monster&fname=${event.target.value.replace(
+          " ",
+          "%20"
+        )}`;
+      }
+      // Call API and set information from it, if no results then set that to be displayed instead
+
+      fetch(searchUrl)
+        .then((res) => res.json())
+        .then((result) => {
+          setOptions(result.data.slice(0, 10));
+        })
+        .catch((error) => {
+          setOptions([{ name: "No results" }]);
+        });
+    }
+  }
+
+  // Set store card image to state and clear searchlist
+  const newImage = (image) => {
+    setCardImage(image);
+    setOptions([]);
+  };
+  // Note: the empty deps array [] means
+  // this useEffect will run once
+  // similar to componentDidMount()
+  useEffect(() => {
+    // fetch("https://db.ygoprodeck.com/api/v7/cardinfo.php?name=Tornado%20Dragon")
+    fetch(
+      "https://db.ygoprodeck.com/api/v7/cardinfo.php?format=tcg&type=fusion%20monster"
+    )
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setCards(result);
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      );
+  }, []);
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return (
+      <div className="loading-div">
+        <p className="loading-text">Loading... </p>
+        <Loading />
+      </div>
+    );
+  } else {
+    cardSelect = Math.floor(Math.random() * cards.data.length);
+    // return <img src={items.data[0].card_images[0].image_url}></img>;
+    return (
+      <Fragment>
+        <Searchbox
+          data={options}
+          // Call debounce function with inputchange so that it doesn't run more than 4 times a second
+          onInputChange={debounce(onInputChange, 250)}
+          newImage={newImage}
+        />
+        {/* <img
+          alt="card"
+          src={cards.data[cardSelect].card_images[0].image_url}
+        ></img> */}
+      </Fragment>
+    );
+  }
 };
 
 export default Game;

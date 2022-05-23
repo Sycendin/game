473a2efddbcd68d393ac8c8f2b00e9a37c2f1dfd
Brandon <37314885+Sycendin@users.noreply.github.com>
2022-05-23 00:09:00 -0400

    style: change sty;e of atk div Background color now extends to whole height of div and an arrow will indicate whether the value is higher or lower than the chosen card's atk/def

--- src/components/Game/SelectedDiv/SelectedChecks/SelectedChecks.js
+++ src/components/Game/SelectedDiv/SelectedChecks/SelectedChecks.js
@@ -4,23 +4,39 @@ const SelectedChecks = ({ item, mainCard }) => {
   console.log(item.atk);
   let atkDifference = "correct";
   let defDifference = "correct";
+  let atkArrow = "";
+  let defArrow = "";
   if (item.atk > mainCard.atk) {
     atkDifference = "higher";
+    atkArrow =
+      "https://cdn.discordapp.com/attachments/958975018816131132/978144886874329118/uparrow.webp";
   } else if (item.atk < mainCard.atk) {
     atkDifference = "lower";
+    atkArrow =
+      "https://cdn.discordapp.com/attachments/958975018816131132/978144887138574396/downarrow.webp";
   }
   if (item.def > mainCard.def) {
     defDifference = "higher";
+    defArrow =
+      "https://cdn.discordapp.com/attachments/958975018816131132/978144886874329118/uparrow.webp";
   } else if (item.def < mainCard.def) {
     defDifference = "lower";
+    defArrow =
+      "https://cdn.discordapp.com/attachments/958975018816131132/978144887138574396/downarrow.webp";
   }
   return (
     <Fragment>
       <div className="selected-checks-div">
-        <p className="selected-checks-text">Atk:</p>
-        <p className={`selected-checks-text-${atkDifference}`}>{item.atk}</p>
-        <p className="selected-checks-text">Def:</p>
-        <p className={`selected-checks-text-${defDifference}`}>{item.def}</p>
+        <p className="selected-checks-text-atk">Atk:</p>
+        <div className={`selected-checks-stats-div-${atkDifference}`}>
+          <p className={`selected-checks-text`}>{item.atk}</p>
+          <img alt="arrow" className={`arrow`} src={atkArrow}></img>
+        </div>
+        <p className="selected-checks-text-def">Def:</p>
+        <div className={`selected-checks-stats-div-${defDifference}`}>
+          <p className={`selected-checks-text`}>{item.def}</p>
+          <img alt="arrow" className={`arrow`} src={defArrow}></img>
+        </div>
       </div>
     </Fragment>
   );

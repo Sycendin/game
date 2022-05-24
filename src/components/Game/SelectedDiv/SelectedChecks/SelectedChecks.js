d5861bee369481bb40ad49c32a17abf3d11fb6d4
Brandon <37314885+Sycendin@users.noreply.github.com>
2022-05-24 00:32:01 -0400

    update/style: update style and add more comparisons Selected list will now also check if the levels,type and attribute match and improve layout of the div

--- src/components/Game/SelectedDiv/SelectedChecks/SelectedChecks.js
+++ src/components/Game/SelectedDiv/SelectedChecks/SelectedChecks.js
@@ -1,11 +1,22 @@
 import React, { Fragment } from "react";
+import AttributeIcon from "../../../Random/CardDiv/RightSideDiv/CardTypeWithIcons/AttributeIcon/AttributeIcon";
+import LevelIcon from "../../../Random/CardDiv/RightSideDiv/CardTypeWithIcons/LevelIcon/LevelIcon";
+import MonsterType from "../../../MonsterType/MonsterType";
+
 import "./SelectedChecks.css";
 const SelectedChecks = ({ item, mainCard }) => {
-  console.log(item.atk);
   let atkDifference = "correct";
   let defDifference = "correct";
-  let atkArrow = "";
-  let defArrow = "";
+  let levelDifference = "correct";
+  let raceDifference = "correct";
+  let attributeDifference = "correct";
+
+  let atkArrow =
+    "https://cdn.discordapp.com/attachments/958975018816131132/978512440943845376/equals.webp";
+  let defArrow =
+    "https://cdn.discordapp.com/attachments/958975018816131132/978512440943845376/equals.webp";
+  let levelArrow =
+    "https://cdn.discordapp.com/attachments/958975018816131132/978512440943845376/equals.webp";
   if (item.atk > mainCard.atk) {
     atkDifference = "higher";
     atkArrow =
@@ -24,6 +35,22 @@ const SelectedChecks = ({ item, mainCard }) => {
     defArrow =
       "https://cdn.discordapp.com/attachments/958975018816131132/978144887138574396/downarrow.webp";
   }
+  if (item.level > mainCard.level) {
+    levelDifference = "higher";
+    levelArrow =
+      "https://cdn.discordapp.com/attachments/958975018816131132/978144886874329118/uparrow.webp";
+  } else if (item.level < mainCard.level) {
+    levelDifference = "lower";
+    levelArrow =
+      "https://cdn.discordapp.com/attachments/958975018816131132/978144887138574396/downarrow.webp";
+  }
+  if (item.race !== mainCard.race) {
+    raceDifference = "wrong";
+  }
+  if (item.attribute !== mainCard.attribute) {
+    attributeDifference = "wrong";
+  }
+
   return (
     <Fragment>
       <div className="selected-checks-div">
@@ -36,6 +63,19 @@ const SelectedChecks = ({ item, mainCard }) => {
         <div className={`selected-checks-stats-div-${defDifference}`}>
           <p className={`selected-checks-text`}>{item.def}</p>
           <img alt="arrow" className={`arrow`} src={defArrow}></img>
+        </div>
+        <div className={`selected-checks-stats-div-${levelDifference}`}>
+          <LevelIcon level={item.level} display={"search"} />
+          <p className={`selected-checks-text`}>{item.level}</p>
+          <img alt="arrow" className={`arrow`} src={levelArrow}></img>
+        </div>
+        <div className={`selected-checks-stats-div-${raceDifference}`}>
+          <MonsterType monsterType={item.race} display={"search"} />
+          <p className={`selected-checks-text`}>{item.race}</p>
+        </div>
+        <div className={`selected-checks-stats-div-${attributeDifference}`}>
+          <AttributeIcon attribute={item.attribute} display={"search"} />
+          <p className={`selected-checks-text`}>{item.attribute}</p>
         </div>
       </div>
     </Fragment>

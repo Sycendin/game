0f0507aad1ca4e8cbe6e4508a17b7bcd71978d08
Brandon <37314885+Sycendin@users.noreply.github.com>
2022-06-12 03:42:07 -0400

    style: improve style for filter

--- src/components/Filter/MonsterTypeFilter/MonsterTypeFilter.js
+++ src/components/Filter/MonsterTypeFilter/MonsterTypeFilter.js
@@ -8,35 +8,37 @@ const MonsterTypeFilter = ({ values }) => {
   };
   return (
     <Fragment>
-      <label class="select-text" htmlFor="monstertype">
-        MonsterType:
-      </label>
-      <select
-        id="monstertype"
-        className="select-text"
-        onChange={() => attributeChange()}
-      >
-        <option value="none">None</option>
-        <option value="Fusion Monster">Fusion</option>
-        <option value="XYZ Monster">XYZ</option>
-        <option value="Synchro Monster, Synchro Tuner Monster, Synchro Pendulum Effect Monster">
-          Synchro
-        </option>
-        <option value="Pendulum Effect Monster, Pendulum Normal Monster, Pendulum Tuner Effect Monster, Pendulum Flip Effect Monster, Pendulum Fusion Monster">
-          Pendulum
-        </option>
-        <option value="Link Monster">Link</option>
-        <option value="Ritual Effect Monster, Ritual Monster">Ritual</option>
-        <option value="Toon Monster">Toon</option>
-        <option value="Flip Effect Monster, Flip Tuner Effect Monster, Flip Monster">
-          Flip
-        </option>
-        <option value="Union Effect Monster">Union</option>
-        <option value="Gemini Monster">Gemini</option>
-        <option value="Spirit Monster">Spirit</option>
-        <option value="Tuner Monster">Tuner</option>
-        <option value="Normal Monster">Normal</option>
-      </select>
+      <div className="filter-option-div">
+        <label class="select-text filter-selector" htmlFor="monstertype">
+          MonsterType:
+        </label>
+        <select
+          id="monstertype"
+          className="select-text"
+          onChange={() => attributeChange()}
+        >
+          <option value="none">None</option>
+          <option value="Fusion Monster">Fusion</option>
+          <option value="XYZ Monster">XYZ</option>
+          <option value="Synchro Monster, Synchro Tuner Monster, Synchro Pendulum Effect Monster">
+            Synchro
+          </option>
+          <option value="Pendulum Effect Monster, Pendulum Normal Monster, Pendulum Tuner Effect Monster, Pendulum Flip Effect Monster, Pendulum Fusion Monster">
+            Pendulum
+          </option>
+          <option value="Link Monster">Link</option>
+          <option value="Ritual Effect Monster, Ritual Monster">Ritual</option>
+          <option value="Toon Monster">Toon</option>
+          <option value="Flip Effect Monster, Flip Tuner Effect Monster, Flip Monster">
+            Flip
+          </option>
+          <option value="Union Effect Monster">Union</option>
+          <option value="Gemini Monster">Gemini</option>
+          <option value="Spirit Monster">Spirit</option>
+          <option value="Tuner Monster">Tuner</option>
+          <option value="Normal Monster">Normal</option>
+        </select>
+      </div>
     </Fragment>
   );
 };
--- src/components/Filter/TypeFilter/TypeFilter.js
+++ src/components/Filter/TypeFilter/TypeFilter.js
@@ -8,40 +8,42 @@ const TypeFilter = ({ values }) => {
   };
   return (
     <Fragment>
-      <label className="select-text" htmlFor="type">
-        Type:
-      </label>
+      <div className="filter-option-div">
+        <label className="select-text" htmlFor="type">
+          Type:
+        </label>
 
-      <select
-        id="type"
-        className="select-text"
-        onChange={() => attributeChange()}
-      >
-        <option value="none">None</option>
-        <option value="Warrior">Warrior</option>
-        <option value="Aqua">Aqua</option>
-        <option value="Beast">Beast</option>
-        <option value="Beast-Warrior">Beast-Warrior</option>
-        <option value="Cyberse">Cyberse</option>
-        <option value="Dinosaur">Dinosaur</option>
-        <option value="Divine-Beast">Divine-Beast</option>
-        <option value="Dragon">Dragon</option>
-        <option value="Fairy">Fairy</option>
-        <option value="Fiend">Fiend</option>
-        <option value="Fish">Fish</option>
-        <option value="Insect">Insect</option>
-        <option value="Machine">Machine</option>
-        <option value="Plant">Plant</option>
-        <option value="Psychic">Psychic</option>
-        <option value="Pyro">Pyro</option>
-        <option value="Reptile">Reptile</option>
-        <option value="Rock">Rock</option>
-        <option value="Sea Serpent">Sea Serpent</option>
-        <option value="Thunder">Thunder</option>
-        <option value="Winged Beast">Winged Beast</option>
-        <option value="Wyrm">Wyrm</option>
-        <option value="Zombie">Zombie</option>
-      </select>
+        <select
+          id="type"
+          className="select-text filter-selector"
+          onChange={() => attributeChange()}
+        >
+          <option value="none">None</option>
+          <option value="Warrior">Warrior</option>
+          <option value="Aqua">Aqua</option>
+          <option value="Beast">Beast</option>
+          <option value="Beast-Warrior">Beast-Warrior</option>
+          <option value="Cyberse">Cyberse</option>
+          <option value="Dinosaur">Dinosaur</option>
+          <option value="Divine-Beast">Divine-Beast</option>
+          <option value="Dragon">Dragon</option>
+          <option value="Fairy">Fairy</option>
+          <option value="Fiend">Fiend</option>
+          <option value="Fish">Fish</option>
+          <option value="Insect">Insect</option>
+          <option value="Machine">Machine</option>
+          <option value="Plant">Plant</option>
+          <option value="Psychic">Psychic</option>
+          <option value="Pyro">Pyro</option>
+          <option value="Reptile">Reptile</option>
+          <option value="Rock">Rock</option>
+          <option value="Sea Serpent">Sea Serpent</option>
+          <option value="Thunder">Thunder</option>
+          <option value="Winged Beast">Winged Beast</option>
+          <option value="Wyrm">Wyrm</option>
+          <option value="Zombie">Zombie</option>
+        </select>
+      </div>
     </Fragment>
   );
 };

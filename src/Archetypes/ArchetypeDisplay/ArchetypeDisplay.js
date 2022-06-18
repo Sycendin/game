c0755ad9e3da6eaaab59d669fd70e652c990d14e
Brandon <37314885+Sycendin@users.noreply.github.com>
2022-06-18 00:03:42 -0400

    update: add intial files for archetype pages

---
+++ src/Archetypes/ArchetypeDisplay/ArchetypeDisplay.js
@@ -0,0 +1,7 @@
+import React, { Fragment } from "react";
+import "./ArchetypeDisplay.css";
+const ArchetypeDisplay = () => {
+  return <Fragment></Fragment>;
+};
+
+export default ArchetypeDisplay;
---
+++ src/Archetypes/Archetypes.js
@@ -0,0 +1,7 @@
+import React, { Fragment } from "react";
+import "./Archetypes.css";
+const Archetypes = () => {
+  return <Fragment></Fragment>;
+};
+
+export default Archetypes;

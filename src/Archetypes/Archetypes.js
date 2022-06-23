605e616c81d05db7ccb879ec9b5f8c5d7f382703
Brandon <37314885+Sycendin@users.noreply.github.com>
2022-06-23 02:42:04 -0400

    feat: can filter archetypes on archetype page

--- src/Archetypes/Archetypes.js
+++ src/Archetypes/Archetypes.js
@@ -1,51 +1,101 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import { data } from "./ArchetypeData";
 import { Link } from "react-router-dom";
 import "./Archetypes.css";
 
 const Archetypes = () => {
+  let aValue = "";
+  const [aFilter, setAFilter] = useState(false);
+  const [update, setUpdate] = useState("");
+  const change = (event) => {
+    setAFilter(true);
+    setUpdate(event.target.value);
+  };
+
   return (
     <Fragment>
       <h1>All Archetypes</h1>
-      <div className="archetype-whole-div">
-        {data.map((element, i) => {
-          return (
-            <Fragment key={i}>
-              <div className="archetype-letter-div">
-                <p className="archetype-letter">{element.letter}</p>
-              </div>
-              <div className="archetype-div" key={i}>
-                {element.archetype.map((element, i) => {
-                  return (
-                    <Fragment key={i}>
-                      <Link
-                        to={`/game/archetypes/${element[0]}`}
-                        style={{ textDecoration: "none" }}
-                      >
-                        <div className="each-archetype-div">
-                          <img
-                            alt="archetype-mascot"
-                            className="archetype-mascot"
-                            src={element[1]}
-                            width={624}
-                            height={624}
-                          ></img>
-                          <div className="archetype-content">
-                            {/* {element[0].split(" ").map((e, i) => {
-                              return <p className="archetype-name">{e}</p>;
-                            })} */}
-                            <p className="archetype-name">{element[0]}</p>
-                          </div>
-                        </div>
-                      </Link>
-                    </Fragment>
-                  );
-                })}
-              </div>
-            </Fragment>
-          );
-        })}
+      <div className="archetype-search-div">
+        <div className="searchbar">
+          <input
+            id="archetype-input"
+            type="text"
+            className="search-box"
+            placeholder="Search"
+            onChange={change}
+          />
+        </div>
       </div>
+      {aFilter === false || update === "" ? (
+        <div className="archetype-whole-div">
+          {data.map((element, i) => {
+            return (
+              <Fragment key={i}>
+                <div className="archetype-letter-div">
+                  <p className="archetype-letter">{element.letter}</p>
+                </div>
+                <div className="archetype-div" key={i}>
+                  {element.archetype.map((element, i) => {
+                    return (
+                      <Fragment key={i}>
+                        <Link
+                          to={`/game/archetypes/${element[0]}`}
+                          style={{ textDecoration: "none" }}
+                        >
+                          <div className="each-archetype-div">
+                            <img
+                              alt="archetype-mascot"
+                              className="archetype-mascot"
+                              src={element[1]}
+                              width={624}
+                              height={624}
+                            ></img>
+                            <div className="archetype-content">
+                              <p className="archetype-name">{element[0]}</p>
+                            </div>
+                          </div>
+                        </Link>
+                      </Fragment>
+                    );
+                  })}
+                </div>
+              </Fragment>
+            );
+          })}
+        </div>
+      ) : (
+        <div className="archetype-whole-div">
+          <div className="archetype-div">
+            {data.map((element, i) => {
+              return element.archetype.map((element, i) => {
+                aValue = document.getElementById("archetype-input").value;
+                let copy = element[0].slice();
+                return copy.toLowerCase().includes(aValue) ? (
+                  <Fragment key={i}>
+                    <Link
+                      to={`/game/archetypes/${element[0]}`}
+                      style={{ textDecoration: "none" }}
+                    >
+                      <div className="each-archetype-div">
+                        <img
+                          alt="archetype-mascot"
+                          className="archetype-mascot"
+                          src={element[1]}
+                          width={624}
+                          height={624}
+                        ></img>
+                        <div className="archetype-content">
+                          <p className="archetype-name">{element[0]}</p>
+                        </div>
+                      </div>
+                    </Link>
+                  </Fragment>
+                ) : null;
+              });
+            })}
+          </div>
+        </div>
+      )}
     </Fragment>
   );
 };

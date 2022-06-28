d6245309fd832929f99023dbc582108e231d015d
Brandon <37314885+Sycendin@users.noreply.github.com>
2022-06-28 01:53:11 -0400

    feat: add links for nav

--- src/Navigation/Navigation.js
+++ src/Navigation/Navigation.js
@@ -1,4 +1,5 @@
 import React, { Fragment } from "react";
+import { Link } from "react-router-dom";
 import "./Navigation.css";
 
 const Navigation = ({ width }) => {
@@ -7,10 +8,30 @@ const Navigation = ({ width }) => {
       <div className="nav-div">
         {width > 600 ? (
           <Fragment>
-            <p className="nav-text">Game</p>
-            <p className="nav-text">Search</p>
-            <p className="nav-text">Random Card</p>
-            <p className="nav-text">All Archetypes</p>
+            <Link
+              to={`/game/archetypes/game`}
+              style={{ textDecoration: "none" }}
+            >
+              <p className="nav-text">Game</p>
+            </Link>
+            <Link
+              to={`/game/archetypes/search`}
+              style={{ textDecoration: "none" }}
+            >
+              <p className="nav-text">Search</p>
+            </Link>
+            <Link
+              to={`/game/archetypes/random`}
+              style={{ textDecoration: "none" }}
+            >
+              <p className="nav-text">Random Card</p>
+            </Link>
+            <Link
+              to={`/game/archetypes/archetypes`}
+              style={{ textDecoration: "none" }}
+            >
+              <p className="nav-text">All Archetypes</p>
+            </Link>
           </Fragment>
         ) : null}
       </div>

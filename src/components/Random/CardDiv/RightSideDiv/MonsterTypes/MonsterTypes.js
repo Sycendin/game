cd904aca5ec950383b6e6d811e78bc00273704ca
Brandon <37314885+Sycendin@users.noreply.github.com>
2022-07-09 05:07:04 -0400

    style/bugfix: fix bug with random button and improve responsive styling

--- src/components/Random/CardDiv/RightSideDiv/MonsterTypes/MonsterTypes.js
+++ src/components/Random/CardDiv/RightSideDiv/MonsterTypes/MonsterTypes.js
@@ -6,8 +6,9 @@ const MonsterTypes = ({ cardData, typeArray }) => {
   } else {
     return (
       <div className="type-div">
+        <p className="type-text">[{cardData.race}/</p>
+        <p className="type-text">{typeArray[0]}</p>
         <p className="type-text">
-          [{cardData.race}/{typeArray[0]}
           {cardData.type === "Fusion Monster" ||
           cardData.type === "XYZ Monster" ||
           cardData.type === "Synchro Monster" ||

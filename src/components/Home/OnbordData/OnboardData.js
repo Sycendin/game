5e423543324731044ff05e9199b650b1c2123ff7
Brandon <37314885+Sycendin@users.noreply.github.com>
2022-07-10 03:31:33 -0400

    feat: add mobile view for onboard

---
+++ src/components/Home/OnbordData/OnboardData.js
@@ -0,0 +1,23 @@
+export const data = [
+  {
+    title: "Game",
+    text: "This is what the game consists of",
+    image:
+      "https://cdn.discordapp.com/attachments/788247984517283880/991961353545855026/troutface.webp",
+    links: [],
+  },
+  {
+    title: "Search",
+    text: "This is what search consists of",
+    image:
+      "https://cdn.discordapp.com/attachments/788247984517283880/991961353545855026/troutface.webp",
+    links: [],
+  },
+  {
+    title: "Random",
+    text: "This is what random consists of",
+    image:
+      "https://cdn.discordapp.com/attachments/788247984517283880/991961353545855026/troutface.webp",
+    links: [],
+  },
+];

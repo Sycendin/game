e1b09320bcd118b5071b0edd7a055c862dcded2c
Brandon <37314885+Sycendin@users.noreply.github.com>
2022-07-10 04:20:57 -0400

    update: add images to onboard

--- src/components/Home/Onboard/Onboard.js
+++ src/components/Home/Onboard/Onboard.js
@@ -5,12 +5,19 @@ const Onboard = () => {
   return (
     <Fragment>
       <div className="onboard-title-div">
-        <p className="onboard-title">Other Projects</p>
+        <p className="onboard-div-title">Pages</p>
       </div>
       {data.map((element, i) => {
         return (
-          <div className="onboard">
+          <div key={i} className="onboard">
             <div className="onboard-info">
+              <img
+                alt={element.title}
+                width={1280}
+                height={720}
+                className="onboard-img"
+                src={data[i].image}
+              ></img>
               <p className="onboard-title">{data[i].title}</p>
               <p className="onboard-text">{data[i].text}</p>
             </div>
--- src/components/Home/OnbordData/OnboardData.js
+++ src/components/Home/OnbordData/OnboardData.js
@@ -3,28 +3,35 @@ export const data = [
     title: "Game Page",
     text: "This is what the game consists of",
     image:
-      "https://cdn.discordapp.com/attachments/788247984517283880/991961353545855026/troutface.webp",
+      "https://cdn.discordapp.com/attachments/747928182891610213/995601841591164928/game720.webp",
     links: [],
   },
   {
     title: "Search Page",
     text: "This is what search consists of",
     image:
-      "https://cdn.discordapp.com/attachments/788247984517283880/991961353545855026/troutface.webp",
+      "https://cdn.discordapp.com/attachments/747928182891610213/995601842048348210/search720.webp",
     links: [],
   },
   {
     title: "Random Page",
     text: "This is what random consists of",
     image:
-      "https://cdn.discordapp.com/attachments/788247984517283880/991961353545855026/troutface.webp",
+      "https://cdn.discordapp.com/attachments/747928182891610213/995601841851203644/random720.webp",
     links: [],
   },
   {
-    title: "Archetype Page",
+    title: "All Archetypes Page",
     text: "This is what random consists of",
     image:
-      "https://cdn.discordapp.com/attachments/788247984517283880/991961353545855026/troutface.webp",
+      "https://cdn.discordapp.com/attachments/747928182891610213/995601841138171936/archetypes720.webp",
+    links: [],
+  },
+  {
+    title: "All Cards In Archetype Page",
+    text: "This is what random consists of",
+    image:
+      "https://cdn.discordapp.com/attachments/747928182891610213/995601841360474132/archetypescards720.webp",
     links: [],
   },
 ];

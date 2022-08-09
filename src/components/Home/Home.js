096f7802921117be50a94378f3ca7727ff51160d
Brandon <37314885+Sycendin@users.noreply.github.com>
2022-08-09 00:05:25 -0400

    update: test markdown

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,19 +1,29 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import Onboard from "./Onboard/Onboard";
 import OnboardM from "./OnboardM/OnboardM";
+import ReactMarkdown from "react-markdown";
+import TermsOfUse from "../test.md";
 import Disqus from "disqus-react";
 import "./Home.css";
 
 const Home = ({ width }) => {
+  const [tosText, setTosText] = useState("");
   const disqusShortname = "yugiohgame";
   const disqusConfig = {
     url: "http://localhost:3000",
     identifier: "homepage",
     title: "Home page",
   };
-
+  useEffect(() => {
+    fetch(TermsOfUse)
+      .then((res) => res.text())
+      .then((text) => setTosText(text));
+  });
   return (
     <Fragment>
+      <div>
+        <ReactMarkdown children={tosText} />
+      </div>
       {width <= 600 ? <OnboardM /> : <Onboard />}
 
       <Disqus.DiscussionEmbed

1a6d5431c870cd891aaff205eb54df5fdce66749
Brandon <37314885+Sycendin@users.noreply.github.com>
2022-08-15 00:08:09 -0400

    update: change post to get

--- src/components/Home/Onboard/OnboardHelper.js
+++ src/components/Home/Onboard/OnboardHelper.js
@@ -1,15 +1,15 @@
 // Get link for markdown file
 export const getData = async (onboard) => {
-  const rawResponse = await fetch("https://yu-game.herokuapp.com/markdown", {
-    method: "POST",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      mdname: onboard,
-    }),
-  });
+  const rawResponse = await fetch(
+    `https://yu-game.herokuapp.com/markdown/${onboard}`,
+    {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    }
+  );
   const content = await rawResponse.json();
   return content[0].link;
 };

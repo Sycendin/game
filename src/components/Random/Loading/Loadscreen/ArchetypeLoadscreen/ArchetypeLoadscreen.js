69cc2465cddf65d16b1fe2db7515bbb1793b96a0
Brandon <37314885+Sycendin@users.noreply.github.com>
2022-10-21 00:04:56 -0400

    update: add more to loadscreen and add responsive features

--- src/components/Random/Loading/Loadscreen/ArchetypeLoadscreen/ArchetypeLoadscreen.js
+++ src/components/Random/Loading/Loadscreen/ArchetypeLoadscreen/ArchetypeLoadscreen.js
@@ -1,12 +1,20 @@
 import React from "react";
 import { Fragment } from "react";
 import { ArchetypeLoadDiv } from "./ArchetypeLoadDiv/ArchetypeLoadDiv";
+import { ArchetypeLoadLetter } from "./ArchetypeLoadLetter/ArchetypeLoadLetter";
 import "./ArchetypeLoadscreen.css";
 export const ArchetypeLoadscreen = () => {
   return (
     <Fragment>
-      <div className="loadscreen min-height">
-        <ArchetypeLoadDiv />
+      <div className="loadscreen min-height fbg">
+        {[...Array(10).keys()].map((element) => {
+          return (
+            <Fragment key={element}>
+              <ArchetypeLoadLetter />
+              <ArchetypeLoadDiv />
+            </Fragment>
+          );
+        })}
       </div>
     </Fragment>
   );

a27761e152e5d9c2e0d7bd3c8400e30e81a676e9
Brandon <37314885+Sycendin@users.noreply.github.com>
2022-10-23 00:13:37 -0400

    update: add more files to home loadscreen

---
+++ src/components/Random/Loading/Loadscreen/HomeLoadScreen/HomeLoadScreenDiv/HomeLoadScreenDiv.js
@@ -0,0 +1,10 @@
+import React, { Fragment } from "react";
+import "./HomeLoadScreenDiv.css";
+export const HomeLoadScreenDiv = () => {
+  return (
+    <Fragment>
+      {" "}
+      <div className="loadscreen min-height fbg"></div>
+    </Fragment>
+  );
+};

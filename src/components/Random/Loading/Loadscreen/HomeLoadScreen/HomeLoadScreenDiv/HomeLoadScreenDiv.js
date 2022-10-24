3c4d166445f65d9b0d1f1edb60de986b5b43cc4f
Brandon <37314885+Sycendin@users.noreply.github.com>
2022-10-24 00:36:35 -0400

    update: add css to home loadscreen

--- src/components/Random/Loading/Loadscreen/HomeLoadScreen/HomeLoadScreenDiv/HomeLoadScreenDiv.js
+++ src/components/Random/Loading/Loadscreen/HomeLoadScreen/HomeLoadScreenDiv/HomeLoadScreenDiv.js
@@ -1,10 +1,15 @@
 import React, { Fragment } from "react";
+import { HomeLoadScreenImage } from "./HomeScreenImage/HomeScreenImage";
+import { HomeLoadScreenText } from "./HomeScreenText/HomeScreenText";
 import "./HomeLoadScreenDiv.css";
 export const HomeLoadScreenDiv = () => {
   return (
     <Fragment>
       {" "}
-      <div className="loadscreen min-height fbg"></div>
+      <div className="homediv fbgd">
+        <HomeLoadScreenImage />
+        <HomeLoadScreenText />
+      </div>
     </Fragment>
   );
 };
